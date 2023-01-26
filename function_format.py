2c35b0c0aa0770427d349f494211dd47d2b36fea
luiluma <luma24lopez@gmail.com>
2023-01-26 18:10:08 -0500

    repaso

---
+++ function_format.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+value1 = 20
+value2 = 30
+print('{}, {}'.format(value1, value2))
+
+# {} son marcadores de posicion.
+#.format, se encarga de formatear una cadena de caracteres. 
---
+++ varibales.py
@@ -0,0 +1,23 @@
+name = "Luisa"
+name = "maria"
+#la variable se re define cada vez que la llamamos
+print(name)
+
+
+number = 10
+number += 1
+#el signo de suma antes de un = quiere decir
+#el valor que ya tiene la variable más lo que este despues del igual 
+print(number)
+
+name = "Luisa"
+Welcome = "Hello " + name + " ¿How are you?"
+#concatenar (unir dos cadenas) se hace de la siguiente forma, sumando lo que queremos unir "concatenar"
+#el espacio es otro caracter
+print (Welcome)
+
+#f string, toma un dato y lo convierte en texto
+
+#operadores de pertenencia (in / not in)
+
+
