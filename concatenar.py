5d58635ed5a88193e41766f6791900a1fa266c17
luiluma <luma24lopez@gmail.com>
2023-01-27 17:59:55 -0500

    concatenacion apuntes

---
+++ concatenar.py
@@ -0,0 +1,12 @@
+#variable
+name = "luisa"
+
+#concatenar con +
+welcome = "hello " + " how are you"
+
+#concatenar con f-string
+welcome = f"hello {nombre} how are you"
+
+#operadores de pertenencia
+print("luisa" in welcome) #true 
+print("luisa" not in welcome) #false
